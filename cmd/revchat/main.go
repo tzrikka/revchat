3b80a02a9eba6af620e8e7fa6ba96d8738ae1aae
Daniel Abraham <aeroquix@gmail.com>
2025-07-28 22:13:22 -0700

    feat: Temporal workflow

--- cmd/revchat/main.go
+++ cmd/revchat/main.go
@@ -1,4 +1,85 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"runtime/debug"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
+	altsrc "github.com/urfave/cli-altsrc/v3"
+	"github.com/urfave/cli/v3"
+
+	"github.com/tzrikka/revchat/pkg/temporal"
+	"github.com/tzrikka/xdg"
+)
+
+const (
+	DirName        = "revchat"
+	ConfigFileName = "config.toml"
+)
+
 func main() {
+	bi, _ := debug.ReadBuildInfo()
+
+	cmd := &cli.Command{
+		Name:    "revchat",
+		Usage:   "Manage code reviews in dedicated chat channels",
+		Version: bi.Main.Version,
+		Flags:   flags(),
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			initLog(cmd.Bool("dev"))
+			return temporal.Run(log.Logger, cmd)
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func flags() []cli.Flag {
+	fs := []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "dev",
+			Usage: "simple setup, but unsafe for production",
+		},
+	}
+
+	path := configFile()
+	fs = append(fs, temporal.Flags(path)...)
+
+	return fs
+}
+
+// configFile returns the path to the app's configuration file.
+// It also creates an empty file if it doesn't already exist.
+func configFile() altsrc.StringSourcer {
+	path, err := xdg.CreateFile(xdg.ConfigHome, DirName, ConfigFileName)
+	if err != nil {
+		log.Fatal().Err(err).Caller().Send()
+	}
+	return altsrc.StringSourcer(path)
+}
+
+// initLog initializes the logger for Timpani's HTTP server and Temporal
+// worker, based on whether it's running in development mode or not.
+func initLog(devMode bool) {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
+
+	if !devMode {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		log.Logger = zerolog.New(os.Stderr).With().Timestamp().Caller().Logger()
+		return
+	}
+
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: "15:04:05.000",
+	}).With().Caller().Logger()
 }
---
+++ pkg/temporal/worker.go
@@ -0,0 +1,35 @@
+// Package temporal initializes a Temporal worker.
+package temporal
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog"
+	"github.com/urfave/cli/v3"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+)
+
+// Run initializes the Temporal worker, and blocks.
+func Run(l zerolog.Logger, cmd *cli.Command) error {
+	addr := cmd.String("temporal-host-port")
+	l.Info().Msgf("Temporal server address: %s", addr)
+
+	c, err := client.Dial(client.Options{
+		HostPort:  addr,
+		Namespace: cmd.String("temporal-namespace"),
+		Logger:    LogAdapter{zerolog: l},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to dial Temporal: %w", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, cmd.String("temporal-task-queue"), worker.Options{})
+
+	if err := w.Run(worker.InterruptCh()); err != nil {
+		return fmt.Errorf("failed to start Temporal worker: %w", err)
+	}
+
+	return nil
+}
