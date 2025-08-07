6733c73f1eaf4c720d9e6fbec3906451a634e4b2
Daniel Abraham <aeroquix@gmail.com>
2025-08-06 19:30:23 -0700

    chore: function docs

--- pkg/data/users.go
+++ pkg/data/users.go
@@ -15,50 +15,71 @@ const (
 	slackPrefix     = "slack"
 )
 
+// AddBitbucketUser saves a 2-way mapping between
+// a Bitbucket user's account ID and email address.
 func AddBitbucketUser(accountID, email string) error {
 	return addUser(bitbucketPrefix, accountID, email)
 }
 
+// AddBitbucketUser saves a 2-way mapping between
+// a GitHub user's username and email address.
 func AddGitHubUser(username, email string) error {
 	return addUser(githubPrefix, username, email)
 }
 
+// AddBitbucketUser saves a 2-way mapping between
+// a Slack user's user ID and email address.
 func AddSlackUser(userID, email string) error {
 	return addUser(slackPrefix, userID, email)
 }
 
+// BitbucketUserEmailByID returns a Bitbucket user's email based on
+// their account ID. Returns an empty string if the user is not found.
 func BitbucketUserEmailByID(accountID string) (string, error) {
 	return userEmailByID(bitbucketPrefix, accountID)
 }
 
+// GitHubUserEmailByID returns a GitHub user's email based on their
+// username. Returns an empty string if the user is not found.
 func GitHubUserEmailByID(username string) (string, error) {
 	return userEmailByID(githubPrefix, username)
 }
 
+// SlackUserEmailByID returns a Slack user's email based on their
+// user ID. Returns an empty string if the user is not found.
 func SlackUserEmailByID(userID string) (string, error) {
 	return userEmailByID(slackPrefix, userID)
 }
 
+// BitbucketUserIDByEmail returns a Bitbucket user's account ID based
+// on their email. Returns an empty string if the user is not found.
 func BitbucketUserIDByEmail(email string) (string, error) {
 	return userIDByEmail(bitbucketPrefix, email)
 }
 
+// GitHubUserIDByEmail returns a GitHub user's username based on
+// their email. Returns an empty string if the user is not found.
 func GitHubUserIDByEmail(email string) (string, error) {
 	return userIDByEmail(githubPrefix, email)
 }
 
+// SlackUserIDByEmail returns a Slack user's user ID based on
+// their email. Returns an empty string if the user is not found.
 func SlackUserIDByEmail(email string) (string, error) {
 	return userIDByEmail(slackPrefix, email)
 }
 
+// RemoveBitbucketUser forgets a Bitbucket user's account ID and email.
 func RemoveBitbucketUser(email string) error {
 	return removeUser(bitbucketPrefix, email)
 }
 
+// RemoveGitHubUser forgets a GitHub user's username and email.
 func RemoveGitHubUser(email string) error {
 	return removeUser(githubPrefix, email)
 }
 
+// RemoveSlackUser forgets a Slack user's user ID and email.
 func RemoveSlackUser(email string) error {
 	return removeUser(slackPrefix, email)
 }
