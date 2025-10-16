14e88d0aa36829b95b6ce598e2027c70df7bfb6c
Daniel Abraham <aeroquix@gmail.com>
2025-10-15 20:33:20 -0700

    chore: more unit tests

--- pkg/bitbucket/pullrequest_test.go
+++ pkg/bitbucket/pullrequest_test.go
@@ -4,6 +4,144 @@ import (
 	"testing"
 )
 
+func TestSwitchSnapshot(t *testing.T) {
+	d := t.TempDir()
+	t.Setenv("XDG_DATA_HOME", d)
+
+	// Initial state.
+	snapshot1 := PullRequest{ID: 1}
+	pr, err := switchSnapshot(nil, "url", snapshot1)
+	if err != nil {
+		t.Fatalf("switchSnapshot() error = %v", err)
+	}
+	if pr != nil {
+		t.Fatalf("switchSnapshot() = %v, want %v", pr, nil)
+	}
+
+	// Replace initial snapshot.
+	snapshot2 := PullRequest{ID: 2}
+	pr, err = switchSnapshot(nil, "url", snapshot2)
+	if err != nil {
+		t.Fatalf("switchSnapshot() error = %v", err)
+	}
+	if pr == nil {
+		t.Fatalf("switchSnapshot() = %v, want %v", pr, snapshot2)
+	}
+	if pr.ID != snapshot1.ID {
+		t.Fatalf("switchSnapshot() = %v, want %v", pr.ID, snapshot1.ID)
+	}
+}
+
+func TestReviewersDiffEmpty(t *testing.T) {
+	prev := PullRequest{}
+	curr := PullRequest{}
+
+	added, removed := reviewersDiff(prev, curr)
+
+	if len(added) != 0 {
+		t.Errorf("reviewersDiff() added = %v, want %v", added, []string{})
+	}
+	if len(removed) != 0 {
+		t.Errorf("reviewersDiff() removed = %v, want %v", removed, []string{})
+	}
+}
+
+func TestReviewersDiffAdded1(t *testing.T) {
+	prev := PullRequest{}
+	curr := PullRequest{
+		Reviewers: []Account{
+			{AccountID: "AAA"},
+		},
+	}
+
+	added, removed := reviewersDiff(prev, curr)
+	if len(added) != 1 || added[0] != "AAA" {
+		t.Errorf("reviewersDiff() added = %v, want %v", added, []string{"AAA"})
+	}
+	if len(removed) != 0 {
+		t.Errorf("reviewersDiff() removed = %v, want %v", removed, []string{})
+	}
+}
+
+func TestReviewersDiffAdded3(t *testing.T) {
+	prev := PullRequest{}
+	curr := PullRequest{
+		Reviewers: []Account{
+			{AccountID: "BBB"},
+			{AccountID: "AAA"},
+			{AccountID: "CCC"},
+		},
+	}
+
+	added, removed := reviewersDiff(prev, curr)
+	if len(added) != 3 || added[0] != "AAA" || added[1] != "BBB" || added[2] != "CCC" {
+		t.Errorf("reviewersDiff() added = %v, want %v", added, []string{"AAA", "BBB", "CCC"})
+	}
+	if len(removed) != 0 {
+		t.Errorf("reviewersDiff() removed = %v, want %v", removed, []string{})
+	}
+}
+
+func TestReviewersDiffRemoved1(t *testing.T) {
+	prev := PullRequest{
+		Reviewers: []Account{
+			{AccountID: "AAA"},
+		},
+	}
+	curr := PullRequest{}
+
+	added, removed := reviewersDiff(prev, curr)
+	if len(added) != 0 {
+		t.Errorf("reviewersDiff() added = %v, want %v", added, []string{})
+	}
+	if len(removed) != 1 || removed[0] != "AAA" {
+		t.Errorf("reviewersDiff() removed = %v, want %v", removed, []string{"AAA"})
+	}
+}
+
+func TestReviewersDiffRemoved3(t *testing.T) {
+	prev := PullRequest{
+		Reviewers: []Account{
+			{AccountID: "BBB"},
+			{AccountID: "AAA"},
+			{AccountID: "CCC"},
+		},
+	}
+	curr := PullRequest{}
+
+	added, removed := reviewersDiff(prev, curr)
+	if len(added) != 0 {
+		t.Errorf("reviewersDiff() added = %v, want %v", added, []string{})
+	}
+	if len(removed) != 3 || removed[0] != "AAA" || removed[1] != "BBB" || removed[2] != "CCC" {
+		t.Errorf("reviewersDiff() removed = %v, want %v", removed, []string{"AAA", "BBB", "CCC"})
+	}
+}
+
+func TestReviewersDiffMixed(t *testing.T) {
+	prev := PullRequest{
+		Reviewers: []Account{
+			{AccountID: "AAA"},
+			{AccountID: "BBB"},
+		},
+	}
+	curr := PullRequest{
+		Reviewers: []Account{
+			{AccountID: "CCC"},
+			{AccountID: "DDD"},
+		},
+	}
+
+	added, removed := reviewersDiff(prev, curr)
+
+	if len(added) != 2 || added[0] != "CCC" || added[1] != "DDD" {
+		t.Errorf("reviewersDiff() added = %v, want %v", added, []string{"Charlie"})
+	}
+	if len(removed) != 2 || removed[0] != "AAA" || removed[1] != "BBB" {
+		t.Errorf("reviewersDiff() removed = %v, want %v", removed, []string{"Alice"})
+	}
+}
+
 func TestHTMLURL(t *testing.T) {
 	tests := []struct {
 		name  string
