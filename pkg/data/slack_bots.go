2054ee8a053cb18b43e267adab652c472a1aeab9
Daniel Abraham <aeroquix@gmail.com>
2025-10-24 23:39:36 -0700

    chore: store Slack bot user IDs in TOML file

--- pkg/data/slack_bots.go
+++ pkg/data/slack_bots.go
@@ -1,38 +1,38 @@
 package data
 
 const (
-	slackBotsFile = "slack_bots.json"
+	slackBotsFile = "slack_bots.toml"
 )
 
 func SetSlackBotUserID(botID, userID string) error {
 	path := dataPath(slackBotsFile)
 
-	m, err := readJSON(path)
+	m, err := readTOML(path)
 	if err != nil {
 		return err
 	}
 
 	m[botID] = userID
-	return writeJSON(path, m)
-}
-
-func GetSlackBotUserID(botID string) (string, error) {
-	m, err := readJSON(dataPath(slackBotsFile))
-	if err != nil {
-		return "", err
-	}
-
-	return m[botID], nil
+	return writeTOML(path, m)
 }
 
 func DeleteSlackBotIDs(botID string) error {
 	path := dataPath(slackBotsFile)
 
-	m, err := readJSON(path)
+	m, err := readTOML(path)
 	if err != nil {
 		return err
 	}
 
 	delete(m, botID)
-	return writeJSON(path, m)
+	return writeTOML(path, m)
+}
+
+func GetSlackBotUserID(botID string) (string, error) {
+	m, err := readTOML(dataPath(slackBotsFile))
+	if err != nil {
+		return "", err
+	}
+
+	return m[botID], nil
 }
